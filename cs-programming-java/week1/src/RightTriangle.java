35d7fb221e859ed11a6beea7f0f4cc7e0b034255
Bui Hoang Anh (Adam) <hoanganh.bui@dzsi.com>
2021-09-20 21:32:37 +0700

    [CS-Programming-Java][Week1] udpate assignment

--- cs-programming-java/week1/src/RightTriangle.java
+++ cs-programming-java/week1/src/RightTriangle.java
@@ -1,12 +1,9 @@
 public class RightTriangle {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
 
-        System.out.println((a >= 0 && b >=0 && c >= 0) &&
-                (a*a == b*b + c*c) ||
-                (b*b == a*a + c*c) ||
-                (c*c == a*a + b*b));
+        System.out.println((a>=0 && b>=0 && c>=0) && ((a*a == b*b + c*c) || (b*b == a*a + c*c) || (c*c == a*a + b*b)));
     }
 }
